35d2720c4d940ae68df4e94f85fad738cefb1d0b
Hyp <hypothermia.frost@gmail.com>
2013-04-07 12:08:56 +0100

    AABB frustum intersection

--- src/rendering/frustumCulling.cpp
+++ src/rendering/frustumCulling.cpp
@@ -5,7 +5,7 @@
 namespace rendering {
 namespace frustumCulling {
 
-vec4f planeNormalize(const vec4f& x){
+static inline vec4f planeNormalize(const vec4f& x){
 	auto norm = 1.0f/x.length3();
 	return x*norm;
 }
@@ -92,13 +92,75 @@ static inline Mask intersect(const FrustumSoA& frustum,const vec4f& sphere){
 #endif
 	return 0xFFffFFff;
 }
-static inline Mask intersect(const FrustumSoA& frustum,const vec4f& aabbMin,const vec4f& aabbMax){
-        return 0xFFffFFff;
-}
 static inline Mask intersect(const Frustum& frustum,const vec4f& sphere){
 	for(int i =0;i<6;++i)
 		if((frustum.planes[i].dot3(sphere) + frustum.planes[i].w) < -sphere.w) return 0;
 	return 0xFFffFFff;
+}
+
+//Use the PN vertex AABB test.
+static inline Mask intersect(const Frustum& frustum,const vec4f& aabbMin,const vec4f& aabbMax) {
+	auto p = aabbMin,n = aabbMax;
+	for(int i = 0; i < 6; i++ ) {
+		auto p = aabbMin;
+		if(frustum.planes[i].x >= 0.f) p.x = aabbMax.x;
+		if(frustum.planes[i].y >= 0.f) p.y = aabbMax.x;
+		if(frustum.planes[i].z >= 0.f) p.z = aabbMax.x;
+		
+		if((frustum.planes[i].dot3(p) + frustum.planes[i].w) < 0.0f) return 0;
+	}
+	return 0xFFffFFff;
+}
+static inline Mask intersect(const FrustumSoA& frustum,const vec4f& aabbMin,const vec4f& aabbMax){
+#ifdef ARPHEG_ARCH_X86
+	auto sp = aabbMin.xyzw;
+	 __m128 aabbMin_xxxx = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(0,0,0,0)); 
+	 __m128 aabbMin_yyyy = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(1,1,1,1)); 
+	 __m128 aabbMin_zzzz = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(2,2,2,2)); 
+	sp = aabbMax.xyzw;
+	 __m128 aabbMax_xxxx = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(0,0,0,0)); 
+	 __m128 aabbMax_yyyy = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(1,1,1,1)); 
+	 __m128 aabbMax_zzzz = _mm_shuffle_ps(sp,sp,_MM_SHUFFLE(2,2,2,2)); 
+	 __m128 zero = vec4f::zero().xyzw;
+
+	 //P vertices for 6 planes.
+	 __m128 p04x,p04y,p04z,p46x,p46y,p46z,mask;
+	 
+	 mask =_mm_cmpge_ps(frustum.planes0_4_x.xyzw,zero);
+	 p04x = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_xxxx),_mm_and_ps(mask,aabbMax_xxxx));//NB: Blend instruction(SSE4) can be utilized instead.
+	 mask =_mm_cmpge_ps(frustum.planes0_4_y.xyzw,zero);
+	 p04y = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_yyyy),_mm_and_ps(mask,aabbMax_yyyy));
+	 mask =_mm_cmpge_ps(frustum.planes0_4_z.xyzw,zero);
+	 p04z = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_zzzz),_mm_and_ps(mask,aabbMax_zzzz));
+	 
+	 __m128 dotA_0123 = vec4f::fma(p04x,frustum.planes0_4_x.xyzw,frustum.planes0_4_w.xyzw);
+	 dotA_0123 = vec4f::fma(p04y,frustum.planes0_4_y.xyzw,dotA_0123);
+	 dotA_0123 = vec4f::fma(p04z,frustum.planes0_4_z.xyzw,dotA_0123);
+
+	 //NB: this is a waste of half register.
+	 mask =_mm_cmpge_ps(frustum.planes4_6_x.xyzw,zero);
+	 p46x = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_xxxx),_mm_and_ps(mask,aabbMax_xxxx));
+	 mask =_mm_cmpge_ps(frustum.planes4_6_y.xyzw,zero);
+	 p46y = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_yyyy),_mm_and_ps(mask,aabbMax_yyyy));
+	 mask =_mm_cmpge_ps(frustum.planes4_6_z.xyzw,zero);
+	 p46z = _mm_or_ps(_mm_andnot_ps(mask,aabbMin_zzzz),_mm_and_ps(mask,aabbMax_zzzz));
+
+	 __m128 dotA_45 = vec4f::fma(p46x,frustum.planes4_6_x.xyzw,frustum.planes4_6_w.xyzw);
+	 dotA_45 = vec4f::fma(p46y,frustum.planes4_6_y.xyzw,dotA_45);
+	 dotA_45 = vec4f::fma(p46z,frustum.planes4_6_z.xyzw,dotA_45);
+
+	 //comparison mask = distance x 6 < 0 x 6
+	 dotA_0123 = _mm_cmplt_ps(dotA_0123, zero);
+	 dotA_45   = _mm_cmplt_ps(dotA_45, zero);
+	 dotA_0123 = _mm_or_ps(dotA_0123,dotA_45);
+	 
+	 //If any masks are 0xFFFF_FFFF return false.
+	 uint32 m = uint32(_mm_movemask_ps(dotA_0123)); //mask is zero if r <= for all planes.
+	 //Is there a better way?
+	 return m == 0? 0xFFffFFff : 0;
+#else
+	return 0xFFffFFff;
+#endif
 }
 
 Culler::Culler(scene::rendering::Service* sce) : frustumCount(0),scene(sce) {}
@@ -143,11 +205,16 @@ void cullAABBs  (Culler* culler,const scene::rendering::EntityGridCell* cell) {
         for(uint32 k = 0,fmask = 1;k < culler->frustumCount;++k,fmask<<=1){
 			const FrustumSoA& frustum = culler->frustums[k];
             for(uint32 i =0;i < uint32(cell->boxFrustumCullers.count[j]);++i){
-                //auto inside = intersect(frustum,block.positions[i]);
-                //masks[i] |= fmask & inside;
+                auto inside = intersect(frustum,block.positions[i].min,block.positions[i].max);
+                masks[i] |= fmask & inside;
             }
         }
-
+        //Filter
+		auto scene = culler->scene;
+        for(uint32 i = 0;i < uint32(cell->boxFrustumCullers.count[j]);++i){
+            if(masks[i] == 0) continue;
+			scene->markVisibleEntity(cell,masks[i],block.entities[i]);
+        }
 	}
 }
 
